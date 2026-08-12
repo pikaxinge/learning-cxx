5e4eedc6131cffbbb3c8f72decde97450119c48f
pikaxinge-sys <2392811793@qq.com>
2026-08-13 00:05:57 +0800

    complete all C++ learning exercises

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -9,7 +9,10 @@ struct Tensor4D {
 
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
+        for (auto i = 0u; i < 4; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -27,7 +30,29 @@ struct Tensor4D {
     // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
-        // TODO: 实现单向广播的加法
+        unsigned int size = 1;
+        for (auto i = 0u; i < 4; ++i) {
+            ASSERT(shape[i] == others.shape[i] || others.shape[i] == 1,
+                   "The other tensor's shape must be broadcastable to this tensor.");
+            size *= shape[i];
+        }
+
+        for (auto index = 0u; index < size; ++index) {
+            auto remaining = index;
+            unsigned int othersIndex = 0;
+            unsigned int othersStride = 1;
+
+            for (auto dimension = 4u; dimension-- > 0;) {
+                auto coordinate = remaining % shape[dimension];
+                remaining /= shape[dimension];
+                if (others.shape[dimension] != 1) {
+                    othersIndex += coordinate * othersStride;
+                }
+                othersStride *= others.shape[dimension];
+            }
+
+            data[index] += others.data[othersIndex];
+        }
         return *this;
     }
 };
--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -18,6 +18,11 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    udim stride = 1;
+    for (std::size_t i = shape.size(); i-- > 0;) {
+        strides[i] = stride;
+        stride *= shape[i];
+    }
     return strides;
 }
 
